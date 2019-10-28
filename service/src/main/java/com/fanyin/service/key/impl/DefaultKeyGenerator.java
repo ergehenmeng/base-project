ea23c66b9f84a69b4e7fda26c336b58e03c344db
二哥很猛 <664956140@qq.com>
2019-10-28 10:19:53 +0800

    key生成日期修改

--- service/src/main/java/com/fanyin/service/key/impl/DefaultKeyGenerator.java
+++ service/src/main/java/com/fanyin/service/key/impl/DefaultKeyGenerator.java
@@ -62,7 +62,7 @@ public class DefaultKeyGenerator implements KeyGenerator {
     static {
         //初始化时间 默认值
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2016, Calendar.NOVEMBER, 1);
+        calendar.set(1990, Calendar.DECEMBER, 29);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
