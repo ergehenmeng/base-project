471e6dd4e528aff6db753249e58bf703ed5929a1
二哥很猛 <664956140@qq.com>
2020-01-07 19:51:45 +0800

    优化ApplicationContext工具类

--- api-web/src/main/java/com/eghm/ApiApplication.java
+++ api-web/src/main/java/com/eghm/ApiApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.ApplicationContextEvent;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -20,10 +22,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableAspectJAutoProxy
 @MapperScan("com.eghm.dao.mapper")
-public class ApiApplication {
+public class ApiApplication implements ApplicationListener<ApplicationContextEvent> {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(ApiApplication.class).bannerMode(Banner.Mode.OFF).web(WebApplicationType.SERVLET).run(args);
-        SpringContextUtil.setApplicationContext(applicationContext);
+        new SpringApplicationBuilder(ApiApplication.class).bannerMode(Banner.Mode.OFF).web(WebApplicationType.SERVLET).run(args);
+    }
+
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationContextEvent event) {
+        SpringContextUtil.setApplicationContext(event.getApplicationContext());
     }
 }
