ae0ac0a56e6a0fc7e666682c8dfbc1cb1dc9fc45
殿小二 <dianxiaoer@mandofin.com>
2020-08-25 18:24:19 +0800

    优化代码

---
+++ service/src/main/java/com/eghm/configuration/annotation/BackstageTag.java
@@ -0,0 +1,14 @@
+package com.eghm.configuration.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 标签 表示该字段不是由前台传递过来而是由后台设置进去,主要作用于dto类上
+ * @author 殿小二
+ * @date 2020/8/25
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+public @interface BackstageTag {
+}
