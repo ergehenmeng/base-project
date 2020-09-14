51aefc934f79a005d703d56769ba3e029318fa27
殿小二 <dianxiaoer@mandofin.com>
2020-09-14 17:47:44 +0800

    优化代码

--- service/src/main/java/com/eghm/queue/TaskHandler.java
+++ service/src/main/java/com/eghm/queue/TaskHandler.java
@@ -27,43 +27,46 @@ public class TaskHandler {
         /**
          * 操作日志 多线程处理
          */
-        private static final ThreadPoolExecutor OPERATE_LOG = new ThreadPoolExecutor(5,10,30,TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(),r -> new Thread(r,TaskConstant.OPERATE_LOG_THREAD),((r, executor) ->  log.warn("操作日志的线程池已满,data:[{}]",((AbstractTask)r).getData())));
+        private static final ThreadPoolExecutor OPERATE_LOG = new ThreadPoolExecutor(5, 10, 30, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), r -> new Thread(r, TaskConstant.OPERATE_LOG_THREAD), ((r, executor) -> log.warn("操作日志的线程池已满,data:[{}]", ((AbstractTask) r).getData())));
 
         /**
          * 登陆日志 多线程处理
          */
-        private static final ThreadPoolExecutor LOGIN_LOG = new ThreadPoolExecutor(1,2,30,TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(),r -> new Thread(r,TaskConstant.LOGIN_LOG_THREAD),(r, executor) -> log.warn("登陆日志的线程池已满,data:[{}]",((AbstractTask)r).getData()));
+        private static final ThreadPoolExecutor LOGIN_LOG = new ThreadPoolExecutor(1, 2, 30, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), r -> new Thread(r, TaskConstant.LOGIN_LOG_THREAD), (r, executor) -> log.warn("登陆日志的线程池已满,data:[{}]", ((AbstractTask) r).getData()));
 
         /**
          * 登陆日志 多线程处理
          */
-        private static final ThreadPoolExecutor EXCEPTION_LOG = new ThreadPoolExecutor(1,2,30,TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(),r -> new Thread(r,TaskConstant.EXCEPTION_LOG_THREAD),(r, executor) -> log.warn("异常日志的线程池已满,data:[{}]",((AbstractTask)r).getData()));
+        private static final ThreadPoolExecutor EXCEPTION_LOG = new ThreadPoolExecutor(1, 2, 30, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), r -> new Thread(r, TaskConstant.EXCEPTION_LOG_THREAD), (r, executor) -> log.warn("异常日志的线程池已满,data:[{}]", ((AbstractTask) r).getData()));
     }
 
     /**
      * 添加操作日志
+     *
      * @param task 任务
      */
-    public void executeOperateLog(AbstractTask<SysOperationLog, OperationLogService> task){
+    public void executeOperateLog(AbstractTask<SysOperationLog, OperationLogService> task) {
         ExecutorHolder.OPERATE_LOG.execute(task);
     }
 
     /**
      * 添加登陆日志
+     *
      * @param task 任务
      */
-    public void executeLoginLog(AbstractTask<LoginRecord, LoginLogService> task){
+    public void executeLoginLog(AbstractTask<LoginRecord, LoginLogService> task) {
         ExecutorHolder.LOGIN_LOG.execute(task);
     }
 
     /**
      * 添加异常日志
+     *
      * @param task 任务
      */
-    public void executeExceptionLog(AbstractTask<ExceptionLog, ExceptionLogService> task){
+    public void executeExceptionLog(AbstractTask<ExceptionLog, ExceptionLogService> task) {
         ExecutorHolder.EXCEPTION_LOG.execute(task);
     }
 }
