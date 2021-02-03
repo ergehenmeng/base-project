d3a45d3f626e41924043fd79ab561b00e46465c3
殿小二 <dianxiaoer@mandofin.com>
2021-02-03 10:40:01 +0800

    简化代码

--- common/src/main/java/com/eghm/common/utils/VersionUtil.java
+++ common/src/main/java/com/eghm/common/utils/VersionUtil.java
@@ -14,50 +14,22 @@ public class VersionUtil {
     private VersionUtil() {
     }
 
-    private static final String[] REPLACE_CHAR = {"v", "version"};
+    private static final String[] REPLACE_CHAR = {"v"};
 
     /**
      * version是否大于等于指定的target版本
+     * 版本号格式必须遵循以下格式,子版本最多两位
+     * 合法: v2.4.3, v2.42.23, 2.43.3
+     * 非法: 2.432.323
      *
-     * @param version 前台传递过来的版本
-     * @param target  后台版本
-     * @return 如果version大于则true 否则false
+     * @param source 原版本号
+     * @param target 目标版本号
+     * @return 如果source大于等于target则true   否则false
      */
-    public static boolean gte(String version, String target) {
-        String s = replace(version);
-        String t = replace(target);
-
-        //版本刚好等于或者主版本等于
-        if (s.startsWith(t)) {
-            return true;
-        }
-
-        String[] versions = s.split("\\.");
-        String[] targets = t.split("\\.");
-        if (versions.length >= targets.length) {
-            return compare(versions, targets);
-        }
-        return compare(targets, versions);
+    public static boolean gte(String source, String target) {
+        return parseInt(source) >= parseInt(target);
     }
 
-    /**
-     * 比较大小 第一个是否大于第二个
-     *
-     * @param source 第一个
-     * @param target 第二个
-     * @return true  source大于target false source小于target
-     */
-    private static boolean compare(String[] source, String[] target) {
-        for (int i = 0; i < target.length; i++) {
-            String v = source[i];
-            String location = target[i];
-            if (Integer.parseInt(v) > Integer.parseInt(location)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     private static String replace(String str) {
         for (String replace : REPLACE_CHAR) {
