7180d603e0dc6cb02e2404050fc896d7b202403c
二哥很猛 <664956140@qq.com>
2022-05-16 23:35:06 +0800

    业务脚本

---
+++ dao/sql/bus.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `scenic`
+(
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `scenic_name`    varchar(50)    DEFAULT NULL COMMENT '景区名称',
+    `level`          tinyint(1) DEFAULT NULL COMMENT '景区等级 5: 5A 4: 4A 3:3A 2:2A 1:A 0:其他',
+    `open_time`      varchar(100)   DEFAULT NULL COMMENT '景区营业时间',
+    `province_id`    bigint(20) DEFAULT NULL COMMENT '省份id',
+    `city_id`        bigint(20) DEFAULT NULL COMMENT '城市id',
+    `county_id`      bigint(20) DEFAULT NULL COMMENT '县区id',
+    `detail_address` varchar(100)   DEFAULT NULL COMMENT '详细地址',
+    `longitude`      decimal(10, 7) DEFAULT NULL COMMENT '经度',
+    `latitude`       decimal(10, 7) DEFAULT NULL COMMENT '纬度',
+    `describe`       varchar(100)   DEFAULT NULL COMMENT '景区描述信息',
+    `cover_url`      varchar(1000)  DEFAULT NULL COMMENT '景区图片',
+    `introduce`      longtext COMMENT '景区详细介绍信息',
+    `add_time`       datetime       DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+    `update_time`    datetime       DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`        bit(1)         DEFAULT b'0' COMMENT '是否删除 0:未删除 1:已删除',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='景区信息表';
+
+
+CREATE TABLE `scenic_ ticket`
+(
+    `id`                bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `scenic_id`         bigint(20) DEFAULT NULL COMMENT '门票所属景区',
+    `title`             varchar(50)  DEFAULT NULL COMMENT '门票名称',
+    `category`          tinyint(2) DEFAULT NULL COMMENT '门票种类 1: 成人票 2: 老人票 3:儿童票',
+    `cover_url`         varchar(500) DEFAULT NULL COMMENT '门票封面图',
+    `line_price`        int(10) DEFAULT '0' COMMENT '划线价',
+    `sale_price`        int(10) DEFAULT '0' COMMENT '销售价',
+    `cost_price`        int(10) DEFAULT '0' COMMENT '成本价',
+    `advance_day`       tinyint(2) unsigned DEFAULT NULL COMMENT '提前多少天购票',
+    `biggest_purchase`  tinyint(2) DEFAULT '1' COMMENT '单次最大购买数量',
+    `start_date`        date         DEFAULT NULL COMMENT '开始预订时间',
+    `end_date`          date         DEFAULT NULL COMMENT '截止预订时间',
+    `stock`             int(10) DEFAULT '0' COMMENT '剩余库存',
+    `sale_num`          int(10) DEFAULT '0' COMMENT '真实销售数量',
+    `total_num`         int(10) DEFAULT '0' COMMENT '总销量=实际销量+虚拟销量',
+    `introduce`         longtext COMMENT '景区介绍',
+    `valid_days`        smallint(3) DEFAULT NULL COMMENT '有效期购买之日起',
+    `effective_date`    date         DEFAULT NULL COMMENT '生效日期(包含)',
+    `expire_date`       date         DEFAULT NULL COMMENT '失效日期(包含)',
+    `use_scope`         smallint(3) DEFAULT NULL COMMENT '使用范围: 1:周一 2:周二 4:周三 8:周四 16:周五 32:周六 64:周日',
+    `verification_type` tinyint(2) DEFAULT NULL COMMENT '核销方式 1:手动核销 2:自动核销 (凌晨自动核销)',
+    `support_refund`    bit(1)       DEFAULT b'1' COMMENT '是否支持退款 1:支持 0:不支持',
+    `real_buy`          bit(1)       DEFAULT b'0' COMMENT '是否实名购票 0:不实名 1:实名',
+    `add_time`          datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+    `update_time`       datetime     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`           bit(1)       DEFAULT b'0' COMMENT '是否删除 0:未删除 1:已删除',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='景区门票信息表';
+
+
+CREATE TABLE `sys_merchant`
+(
+    `id`               bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `title`            varchar(30)  DEFAULT NULL COMMENT '商家名称',
+    `type`             smallint(4) DEFAULT NULL COMMENT '商家类型: 1:景区 2: 民宿 4: 餐饮 8: 特产 16: 线路',
+    `contact_name`     varchar(10)  DEFAULT NULL COMMENT '联系人姓名',
+    `contact_phone`    varchar(20)  DEFAULT NULL COMMENT '联系人电话',
+    `user_name`        varchar(30)  DEFAULT NULL COMMENT '账号名称',
+    `pwd`              varchar(255) DEFAULT NULL COMMENT '账号密码',
+    `init_pwd`         bit(1)       DEFAULT b'1' COMMENT '是否为初始化密码 1:是 0:不是',
+    `state`            tinyint(1)   DEFAULT '1' COMMENT '状态 0:锁定 1:正常 2:销户',
+    `add_time`         datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time`      datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`          bit(1)       DEFAULT b'0' COMMENT '是否为删除状态 0:未删除 1:已删除',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商家信息表';
+
+
+
