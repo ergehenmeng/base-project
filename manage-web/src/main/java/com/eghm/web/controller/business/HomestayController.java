fcbfde3f791505aed26bc171cb6626dbd8be929c
wangyanbing <wangyanbing@lswooks.com>
2022-07-01 18:13:07 +0800

    post

--- manage-web/src/main/java/com/eghm/web/controller/business/HomestayController.java
+++ manage-web/src/main/java/com/eghm/web/controller/business/HomestayController.java
@@ -13,10 +13,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author 二哥很猛
@@ -36,21 +33,21 @@ public class HomestayController {
         return PageData.toPage(byPage);
     }
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     @ApiOperation("创建民宿")
     public RespBody<Void> create(@RequestBody @Validated HomestayAddRequest request) {
         homestayService.create(request);
         return RespBody.success();
     }
 
-    @GetMapping("/update")
+    @PostMapping("/update")
     @ApiOperation("更新民宿")
     public RespBody<Void> update(@RequestBody @Validated HomestayEditRequest request) {
         homestayService.update(request);
         return RespBody.success();
     }
 
-    @GetMapping("/updateState")
+    @PostMapping("/updateState")
     @ApiOperation("更新上下架状态")
     public RespBody<Void> updateState(@RequestBody @Validated IdStateDTO dto) {
         homestayService.updateState(dto.getId(), dto.getState());
