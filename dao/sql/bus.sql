d7efb2d6a8e0f224e0320d8382987f3183f05005
wangyanbing <wangyanbing@lswooks.com>
2022-07-27 19:25:52 +0800

    脚本更新

--- dao/sql/bus.sql
+++ dao/sql/bus.sql
@@ -417,3 +417,19 @@ CREATE TABLE `activity`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='活动信息表';
 
+CREATE TABLE `pay_notify_log`
+(
+    `id`            bigint(20) NOT NULL COMMENT '主键',
+    `trade_type`    varchar(30) DEFAULT NULL COMMENT '交易方式 WECHAT:微信 ALIPAY:支付宝',
+    `notify_id`     varchar(50) DEFAULT NULL COMMENT '异步通知唯一ID',
+    `notify_type`   varchar(30) DEFAULT NULL COMMENT '通知类型 PAY: 支付异步通知 REFUND:退款异步通知',
+    `out_trade_no`  varchar(30) DEFAULT NULL COMMENT '交易流水号',
+    `out_refund_no` varchar(30) DEFAULT NULL COMMENT '退款流水号',
+    `params`        text COMMENT '退款通知原始参数',
+    `add_time`      datetime    DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+    `update_time`   datetime    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`       bit(1)      DEFAULT b'0' COMMENT '删除状态 0:未删除 1:已删除',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `notify_id_unique` (`trade_type`, `notify_id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='支付异步通知记录表';
