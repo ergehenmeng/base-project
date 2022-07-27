ec814984a45c89150c76ef187ef1ce7fe52a90e2
二哥很猛 <664956140@qq.com>
2022-07-27 23:09:54 +0800

    备注优化

--- service/src/main/java/com/eghm/service/business/TicketOrderService.java
+++ service/src/main/java/com/eghm/service/business/TicketOrderService.java
@@ -10,6 +10,12 @@ public interface TicketOrderService {
 
     /**
      * 创建门票订单
+     * 1. 校验门票信息
+     * 2. 校验优惠券信息(如果有的话)
+     * 3. 创建订单
+     * 4. 增加联系人(如果有的话)
+     * 5. 更新库存
+     * 6. MQ门票订单超时处理
      * @param dto 下单信息
      */
     void create(CreateTicketOrderDTO dto);
