eaa339d06c1fafbff421e05992aa86983c430a8d
wangyanbing <wangyanbing@lswooks.com>
2022-08-13 11:58:51 +0800

    优化代码

--- service/src/main/java/com/eghm/service/mq/listener/RabbitListenerHandler.java
+++ service/src/main/java/com/eghm/service/mq/listener/RabbitListenerHandler.java
@@ -2,10 +2,15 @@ package com.eghm.service.mq.listener;
 
 import com.eghm.common.constant.QueueConstant;
 import com.eghm.service.business.CommonService;
+import com.rabbitmq.client.Channel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.function.Consumer;
 
 /**
  * @author 二哥很猛
@@ -23,8 +28,28 @@ public class RabbitListenerHandler {
      * @param orderNo 订单编号
      */
     @RabbitListener(queues = QueueConstant.ORDER_PAY_EXPIRE_QUEUE)
-    public void orderExpire(String orderNo) {
-        commonService.getOrderService(orderNo).orderExpire(orderNo);
+    public void orderExpire(String orderNo, Message message, Channel channel) throws IOException {
+        processMessageAck(orderNo, message, channel, s -> commonService.getOrderService(s).orderExpire(s));
+    }
+
+
+    /**
+     * 处理MQ中消息,并手动确认
+     * @param msg 消息
+     * @param message message
+     * @param channel channel
+     * @param consumer 业务
+     * @param <T> 消息类型
+     * @throws IOException e
+     */
+    public static <T> void processMessageAck(T msg, Message message, Channel channel, Consumer<T> consumer) throws IOException {
+        try {
+            consumer.accept(msg);
+        } catch (Exception e) {
+            log.error("队列[{}]处理消息异常 [{}] [{}]", message.getMessageProperties().getConsumerQueue(), msg, message, e);
+        } finally {
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        }
     }
 
 }
