e2f63ae7f5ca8c833843953fddbf497c222a3be7
殿小二 <dianxiaoer@mandofin.com>
2023-03-06 14:17:13 +0800

    优化代码

---
+++ service/src/main/java/com/eghm/service/business/ItemService.java
@@ -0,0 +1,9 @@
+package com.eghm.service.business;
+
+/**
+ * @author 殿小二
+ * @date 2023/3/6
+ */
+public interface ItemService {
+    
+}
---
+++ service/src/main/java/com/eghm/utils/BeanValidator.java
@@ -0,0 +1,41 @@
+package com.eghm.utils;
+
+import cn.hutool.core.collection.CollUtil;
+import com.eghm.common.enums.ErrorCode;
+import com.eghm.common.exception.BusinessException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author 殿小二
+ * @date 2023/3/6
+ */
+public class BeanValidator {
+    
+    private static ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    
+    private static <T> void validate(T t, Class<?>... groups) {
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<T>> validate = validator.validate(t, groups);
+        if (CollUtil.isNotEmpty(validate)) {
+            Iterator<ConstraintViolation<T>> iterator = validate.iterator();
+            if (iterator.hasNext()) {
+                ConstraintViolation<T> next = iterator.next();
+                throw new BusinessException(ErrorCode.PARAM_VERIFY_ERROR.getCode(), next.getMessage());
+            }
+        }
+    }
+    
+    public static void validateList(Collection<?> collection) {
+        for (Object o : collection) {
+            validate(o);
+        }
+    }
+ 
+}
