f625ce9cc1cb29a9eac93585249db53c09b06d4b
wangyanbing <wangyanbing@lswooks.com>
2023-03-13 20:54:15 +0800

    优化代码

--- service/src/main/java/com/eghm/configuration/rabbit/RabbitMQConfig.java
+++ service/src/main/java/com/eghm/configuration/rabbit/RabbitMQConfig.java
@@ -4,10 +4,10 @@ import com.eghm.common.enums.RabbitQueue;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 
 /**
  * mq配置
@@ -15,28 +15,29 @@ import javax.annotation.PostConstruct;
  * @author 二哥很猛 2022/6/10 11:21
  */
 @Configuration
-@AllArgsConstructor
 @Slf4j
+@AutoConfigureAfter(RabbitAutoConfiguration.class)
+@AllArgsConstructor
 public class RabbitMQConfig {
 
-    private final RabbitAdmin rabbitAdmin;
+    private final AmqpAdmin amqpAdmin;
 
-    @PostConstruct
+    @Bean
     public void init() {
-        log.info("****开始初始化rabbit消息队列****");
+        log.info("****初始化rabbit消息队列开始****");
         for (RabbitQueue value : RabbitQueue.values()) {
             Queue queue = QueueBuilder.durable(value.getQueue()).build();
-            rabbitAdmin.declareQueue(queue);
+            amqpAdmin.declareQueue(queue);
             String exchangeType = value.getExchangeType().name().toLowerCase();
             ExchangeBuilder builder = new ExchangeBuilder(value.getExchange(), exchangeType).durable(true);
             if (value.isDelayed()) {
                 builder.delayed().withArgument("x-delayed-type", exchangeType);
             }
             Exchange exchange = builder.build();
-            rabbitAdmin.declareExchange(exchange);
-            rabbitAdmin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(value.getRoutingKey()).noargs());
+            amqpAdmin.declareExchange(exchange);
+            amqpAdmin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(value.getRoutingKey()).noargs());
         }
-        log.info("****初始化rabbit消息队列成功****");
+        log.info("****初始化rabbit消息队列结束****");
     }
 
 }
