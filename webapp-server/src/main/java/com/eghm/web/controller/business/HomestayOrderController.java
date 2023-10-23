fcbef1cd595a24937279381304406eef09d3d4f9
wangyanbing <ergehenmeng@qq.com>
2023-10-23 18:13:16 +0800

    民宿下单

---
+++ webapp-server/src/main/java/com/eghm/web/controller/business/HomestayOrderController.java
@@ -0,0 +1,50 @@
+package com.eghm.web.controller.business;
+
+import com.eghm.dto.business.order.ticket.TicketOrderQueryDTO;
+import com.eghm.dto.ext.ApiHolder;
+import com.eghm.dto.ext.RespBody;
+import com.eghm.service.business.TicketOrderService;
+import com.eghm.vo.business.order.ticket.TicketOrderDetailVO;
+import com.eghm.vo.business.order.ticket.TicketOrderVO;
+import com.eghm.web.annotation.AccessToken;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author 二哥很猛
+ * @since 2023/7/31
+ */
+@AccessToken
+@RestController
+@Api(tags = "民宿订单")
+@AllArgsConstructor
+@RequestMapping("/webapp/homestay/order")
+public class HomestayOrderController {
+
+    private final TicketOrderService ticketOrderService;
+
+    @GetMapping("/listPage")
+    @ApiOperation("民宿订单列表")
+    public RespBody<List<TicketOrderVO>> listPage(@Validated TicketOrderQueryDTO dto) {
+        dto.setMemberId(ApiHolder.getMemberId());
+        List<TicketOrderVO> voList = ticketOrderService.getByPage(dto);
+        return RespBody.success(voList);
+    }
+
+    @GetMapping("/detail")
+    @ApiOperation("民宿订单详情")
+    @ApiImplicitParam(name = "orderNo", value = "订单编号", required = true)
+    public RespBody<TicketOrderDetailVO> detail(@RequestParam("orderNo") String orderNo) {
+        TicketOrderDetailVO detail = ticketOrderService.getDetail(orderNo, ApiHolder.getMemberId());
+        return RespBody.success(detail);
+    }
+}
