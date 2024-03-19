997f5bea9b39d2150cc84970da7d73183c19be5f
wangyanbing <ergehenmeng@qq.com>
2024-03-19 15:47:43 +0800

    强迫症

--- service/src/main/java/com/eghm/mapper/AppVersionMapper.java
+++ service/src/main/java/com/eghm/mapper/AppVersionMapper.java
@@ -12,31 +12,23 @@ import java.util.List;
 public interface AppVersionMapper extends BaseMapper<AppVersion> {
 
     /**
-     * 获取最新已上架的版本信息
-     *
-     * @param classify App类型 ANDROID IOS
-     * @return 版本信息
-     */
-    AppVersion getLatestVersion(@Param("classify") String classify);
-
-    /**
      * 获取已上架版本的信息说明
      *
-     * @param classify app类型
+     * @param channel app类型
      * @param version  版本号
      * @return 版本信息
      */
-    AppVersion getVersion(@Param("classify") String classify, @Param("version") String version);
+    AppVersion getVersion(@Param("channel") String channel, @Param("version") String version);
 
     /**
      * 在指定时间段内获取强制更新的版本列表
      *
-     * @param classify     app类型
+     * @param channel     app类型
      * @param startVersion 开始版本
      * @param endVersion   结束版本
      * @return 强制更新的版本列表
      */
-    List<AppVersion> getForceUpdateVersion(@Param("classify") String classify,
+    List<AppVersion> getForceUpdateVersion(@Param("channel") String channel,
                                            @Param("startVersion") Integer startVersion,
                                            @Param("endVersion") Integer endVersion);
 }
