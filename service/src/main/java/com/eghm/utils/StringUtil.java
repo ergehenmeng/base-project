48c3643398b9219513cfabac92d86083fffe6933
wangyanbing <ergehenmeng@qq.com>
2024-05-23 17:52:58 +0800

    调试优化代码

--- service/src/main/java/com/eghm/utils/StringUtil.java
+++ service/src/main/java/com/eghm/utils/StringUtil.java
@@ -257,10 +257,10 @@ public class StringUtil {
 
 
     public static void main(String[] args) {
-        int pid = 101023;
+        int pid = 102113;
         StringBuilder builder = new StringBuilder();
         builder.append("\r\n");
-        builder.append(sql(pid, "替换", 1010, 1, 60)).append("\r\n");
+        builder.append(sql(pid, "替换", 0, 1, 45)).append("\r\n");
 
         int start = Integer.parseInt(pid + "10");
         int index = 1;
