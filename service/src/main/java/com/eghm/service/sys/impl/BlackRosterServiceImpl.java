5b4e57896f140c3c9fcac2d991fd5ffdc8e74745
二哥很猛 <664956140@qq.com>
2024-05-25 17:26:04 +0800

    查询优化

--- service/src/main/java/com/eghm/service/sys/impl/BlackRosterServiceImpl.java
+++ service/src/main/java/com/eghm/service/sys/impl/BlackRosterServiceImpl.java
@@ -6,6 +6,7 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.eghm.cache.CacheService;
 import com.eghm.constant.CacheConstant;
 import com.eghm.dto.ext.PagingQuery;
 import com.eghm.dto.roster.BlackRosterAddRequest;
@@ -13,7 +14,6 @@ import com.eghm.enums.ErrorCode;
 import com.eghm.exception.BusinessException;
 import com.eghm.mapper.BlackRosterMapper;
 import com.eghm.model.BlackRoster;
-import com.eghm.cache.CacheService;
 import com.eghm.service.sys.BlackRosterService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,12 +38,13 @@ public class BlackRosterServiceImpl implements BlackRosterService {
     public Page<BlackRoster> getByPage(PagingQuery request) {
         LambdaQueryWrapper<BlackRoster> wrapper = Wrappers.lambdaQuery();
         if (StrUtil.isNotBlank(request.getQueryName())) {
-            if (!PatternPool.IPV4.matcher(request.getQueryName()).matches()) {
-                throw new BusinessException(ErrorCode.IP_ILLEGAL);
+            if (PatternPool.IPV4.matcher(request.getQueryName()).matches()) {
+                long aLong = Ipv4Util.ipv4ToLong(request.getQueryName());
+                wrapper.ge(BlackRoster::getEndIp, aLong);
+                wrapper.le(BlackRoster::getStartIp, aLong);
+            } else {
+                wrapper.like(BlackRoster::getRemark, request.getQueryName());
             }
-            long aLong = Ipv4Util.ipv4ToLong(request.getQueryName());
-            wrapper.ge(BlackRoster::getEndIp, aLong);
-            wrapper.le(BlackRoster::getStartIp, aLong);
         }
         return blackRosterMapper.selectPage(request.createPage(), wrapper);
     }
