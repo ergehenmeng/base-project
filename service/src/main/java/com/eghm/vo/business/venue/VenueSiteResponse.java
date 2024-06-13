9eed90ce400664e5bd1cdd330cff910abe96b1e0
wangyanbing <ergehenmeng@qq.com>
2024-06-13 18:12:48 +0800

    调试优化代码

--- service/src/main/java/com/eghm/vo/business/venue/VenueSiteResponse.java
+++ service/src/main/java/com/eghm/vo/business/venue/VenueSiteResponse.java
@@ -17,8 +17,14 @@ public class VenueSiteResponse {
     @ApiModelProperty("id主键")
     private Long id;
 
+    @ApiModelProperty(value = "场地封面图片")
+    private String coverUrl;
+
     @ApiModelProperty(value = "场地名称")
     private String title;
+
+    @ApiModelProperty(value = "所属场馆")
+    private String venueName;
 
     @ApiModelProperty(value = "状态 0:待上架 1:已上架 2:强制下架")
     private Boolean state;
