d4bde9dc54d31c2dbd02a1110df54e7e14cb088c
wangyanbing <664956140@qq.com>
2024-08-01 11:44:32 +0800

    校验

--- service/src/main/java/com/eghm/model/ItemStore.java
+++ service/src/main/java/com/eghm/model/ItemStore.java
@@ -2,17 +2,20 @@ package com.eghm.model;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.eghm.enums.ref.State;
+import com.eghm.validation.annotation.Phone;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
  * <p>
  * 店铺信息表
  * </p>
- *
+ * 校验不能删除,上架会使用
  * @author 二哥很猛
  * @since 2022-07-01
  */
@@ -22,6 +25,7 @@ import java.math.BigDecimal;
 public class ItemStore extends BaseEntity {
 
     @ApiModelProperty("店铺名称")
+    @NotBlank(message = "店铺名称不能为空")
     private String title;
 
     @ApiModelProperty(value = "状态 0:待上架 1:已上架 2:强制下架")
@@ -31,36 +35,47 @@ public class ItemStore extends BaseEntity {
     private Long merchantId;
 
     @ApiModelProperty(value = "店铺logo")
+    @NotBlank(message = "店铺logo不能为空")
     private String logoUrl;
 
     @ApiModelProperty("封面图")
+    @NotBlank(message = "店铺封面图不能为空")
     private String coverUrl;
 
     @ApiModelProperty(value = "营业时间")
+    @NotBlank(message = "营业时间不能为空")
     private String openTime;
 
     @ApiModelProperty(value = "省id")
+    @NotNull(message = "省份不能为空")
     private Long provinceId;
 
     @ApiModelProperty(value = "城市id")
+    @NotNull(message = "城市不能为空")
     private Long cityId;
 
     @ApiModelProperty(value = "县区id")
+    @NotNull(message = "县区不能为空")
     private Long countyId;
 
     @ApiModelProperty(value = "详细地址")
+    @NotBlank(message = "详细地址不能为空")
     private String detailAddress;
 
     @ApiModelProperty(value = "经度")
+    @NotNull(message = "经度不能为空")
     private BigDecimal longitude;
 
     @ApiModelProperty(value = "纬度")
+    @NotNull(message = "纬度不能为空")
     private BigDecimal latitude;
 
     @ApiModelProperty(value = "商家电话")
+    @Phone(message = "商家电话格式错误")
     private String telephone;
 
     @ApiModelProperty(value = "商家介绍")
+    @NotBlank(message = "商家介绍不能为空")
     private String introduce;
 
     @ApiModelProperty("是否为推荐店铺 true:是 false:不是")
@@ -70,5 +85,6 @@ public class ItemStore extends BaseEntity {
     private BigDecimal score;
 
     @ApiModelProperty("仓库地址id")
+    @NotNull(message = "仓库地址不能为空")
     private Long depotAddressId;
 }
--- service/src/main/java/com/eghm/model/Restaurant.java
+++ service/src/main/java/com/eghm/model/Restaurant.java
@@ -6,13 +6,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
  * <p>
  * 餐饮商家信息表
  * </p>
- *
+ * 校验不能删除,上架会使用
  * @author 二哥很猛
  * @since 2022-06-30
  */
@@ -22,6 +24,7 @@ import java.math.BigDecimal;
 public class Restaurant extends BaseEntity {
 
     @ApiModelProperty(value = "商家名称")
+    @NotBlank(message = "商家名称不能为空")
     private String title;
 
     @ApiModelProperty(value = "所属商户")
@@ -31,36 +34,47 @@ public class Restaurant extends BaseEntity {
     private State state;
 
     @ApiModelProperty("商家logo")
+    @NotBlank(message = "logo不能为空")
     private String logoUrl;
 
     @ApiModelProperty(value = "商家封面")
+    @NotBlank(message = "封面图片不能为空")
     private String coverUrl;
 
     @ApiModelProperty(value = "营业时间")
+    @NotBlank(message = "营业时间不能为空")
     private String openTime;
 
     @ApiModelProperty(value = "省份")
+    @NotNull(message = "省份不能为空")
     private Long provinceId;
 
     @ApiModelProperty(value = "城市id")
+    @NotNull(message = "城市不能为空")
     private Long cityId;
 
     @ApiModelProperty(value = "县区id")
+    @NotNull(message = "县区不能为空")
     private Long countyId;
 
     @ApiModelProperty("详细地址")
+    @NotBlank(message = "详细地址不能为空")
     private String detailAddress;
 
     @ApiModelProperty(value = "经度")
+    @NotNull(message = "经度不能为空")
     private BigDecimal longitude;
 
     @ApiModelProperty(value = "纬度")
+    @NotNull(message = "纬度不能为空")
     private BigDecimal latitude;
 
     @ApiModelProperty(value = "商家热线")
+    @NotNull(message = "商家热线不能为空")
     private String phone;
 
     @ApiModelProperty(value = "商家介绍")
+    @NotNull(message = "商家介绍不能为空")
     private String introduce;
 
     @ApiModelProperty("分数")
