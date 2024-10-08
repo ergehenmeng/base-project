1017435c96cae6d0b10de7f12d7dc808a61f9589
wangyanbing <664956140@qq.com>
2024-10-08 11:28:56 +0800

    优化

--- service/src/main/java/com/eghm/configuration/annotation/SkipLogger.java
+++ service/src/main/java/com/eghm/configuration/annotation/SkipLogger.java
@@ -9,8 +9,9 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * api项目默认记录所有请求日志,如需不添加日志记录,则添加该注解
- *
+ * 管理后台项目默认记录POST请求日志<br/>
+ * 移动端默认记录全部日志<br/>
+ * 如需不添加日志记录,则添加该注解(移动端和管理后台均有效)
  * @author 二哥很猛
  */
 @Documented
