1d9264c1e5ad31d45ca71bafaa165afa6c3adce0
wangyanbing <664956140@qq.com>
2024-10-09 15:46:37 +0800

    脚本

--- service/sql/base_schema.sql
+++ service/sql/base_schema.sql
@@ -991,7 +991,8 @@ CREATE TABLE `voucher`
     `create_time`   datetime      DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time`   datetime      DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `deleted`       bit(1)        DEFAULT b'0' COMMENT '删除状态 0:未删除 1:已删除',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY `idx` (`restaurant_id`,`tag_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='餐饮代金券';
 
