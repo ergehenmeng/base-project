7983345295222ac0f2bc2e833cb36e378b5918a1
wangyanbing <664956140@qq.com>
2024-10-17 15:33:49 +0800

    会员标签字段绑定

--- service/src/main/java/com/eghm/dto/member/tag/MemberTagEditRequest.java
+++ service/src/main/java/com/eghm/dto/member/tag/MemberTagEditRequest.java
@@ -1,8 +1,11 @@
 package com.eghm.dto.member.tag;
 
+import com.eghm.convertor.YuanToCentDecoder;
 import com.eghm.enums.Channel;
+import com.eghm.validation.annotation.OptionInt;
 import com.eghm.validation.annotation.WordChecker;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -42,14 +45,17 @@ public class MemberTagEditRequest {
     private Integer consumeDay;
 
     @ApiModelProperty(value = "最低消费次数")
+    @JsonDeserialize(using = YuanToCentDecoder.class)
     private Integer consumeNum;
 
     @ApiModelProperty(value = "最低消费金额")
+    @JsonDeserialize(using = YuanToCentDecoder.class)
     private Integer consumeAmount;
 
     @ApiModelProperty("注册渠道 PC ANDROID IOS H5 OTHER")
     private Channel channel;
 
-    @ApiModelProperty("性别 性别 0:未知 1:男 2:女 ")
+    @ApiModelProperty("性别 0:未知 1:男 2:女 ")
+    @OptionInt(value = {0, 1, 2}, required = false, message = "性别参数错误")
     private Integer sex;
 }
