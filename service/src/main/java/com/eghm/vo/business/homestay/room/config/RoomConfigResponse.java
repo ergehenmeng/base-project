3a654cc82394bfc72c51dd74092e7e31fe8717df
wangyanbing <664956140@qq.com>
2024-11-28 15:26:45 +0800

    删除多余代码

--- service/src/main/java/com/eghm/vo/business/homestay/room/config/RoomConfigResponse.java
+++ service/src/main/java/com/eghm/vo/business/homestay/room/config/RoomConfigResponse.java
@@ -2,15 +2,12 @@ package com.eghm.vo.business.homestay.room.config;
 
 import com.eghm.convertor.CentToYuanOmitEncoder;
 import com.eghm.vo.business.BaseConfigResponse;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 /**
  * @author 二哥很猛
@@ -24,10 +21,6 @@ public class RoomConfigResponse extends BaseConfigResponse {
 
     @ApiModelProperty("是否可订 false:不可订 true:可定")
     private Boolean state;
-
-    @ApiModelProperty("日期")
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate configDate;
 
     @ApiModelProperty("划线价")
     @JsonSerialize(using = CentToYuanOmitEncoder.class)
