f84d017b21a3d1725a5da6878e5b099c48ce6980
wangyanbing <664956140@qq.com>
2024-12-02 09:42:14 +0800

    sms-tips

--- service/src/main/java/com/eghm/enums/TemplateType.java
+++ service/src/main/java/com/eghm/enums/TemplateType.java
@@ -21,29 +21,29 @@ public enum TemplateType implements EnumBinder<String> {
     DEFAULT("default", "自定义短信", "", ""),
 
     /**
-     * 登陆发送短信
+     * 登陆
      */
-    MEMBER_LOGIN("member_login", "登陆发送短信", "", "您正在登陆EGHM平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
+    MEMBER_LOGIN("member_login", "登陆", "", "您正在登陆EGHM平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 登陆发送短信
+     * 登陆(后台)
      */
-    USER_LOGIN("user_login", "登陆发送短信", "", "您正在登陆EGHM管理平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
+    USER_LOGIN("user_login", "登陆(后台)", "", "您正在登陆EGHM管理平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 忘记登陆密码
+     * 忘记密码
      */
-    FORGET("forget", "忘记密码发送短信", "", "您正在修改EGHM平台登录密码，短信验证码：${param0}。如非本人操作，请忽略此短信"),
+    FORGET("forget", "忘记密码", "", "您正在修改EGHM平台登录密码，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 注册发送短信
+     * 注册
      */
-    REGISTER("register", "注册发送短信", "", "您正在注册EGHM平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
+    REGISTER("register", "注册", "", "您正在注册EGHM平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 更换邮箱发送短信验证码
+     * 更换邮箱
      */
-    CHANGE_EMAIL("change_email", "更换邮箱发送短信验证码", "", "您正在更换EGHM平台账号的邮箱，短信验证码：${param0}。如非本人操作，请忽略此短信"),
+    CHANGE_EMAIL("change_email", "更换邮箱", "", "您正在更换EGHM平台账号的邮箱，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     ;
     @EnumValue
