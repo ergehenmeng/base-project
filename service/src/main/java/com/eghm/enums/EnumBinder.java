c25a0b51be7ae8f3e4a45e3ff888a5ad9dfc675c
wangyanbing <664956140@qq.com>
2025-01-02 16:14:13 +0800

    增加注释

--- service/src/main/java/com/eghm/enums/EnumBinder.java
+++ service/src/main/java/com/eghm/enums/EnumBinder.java
@@ -6,7 +6,16 @@ package com.eghm.enums;
  */
 public interface EnumBinder<T> {
 
+    /**
+     * @return 保存数据库或前台使用的值
+     */
     T getValue();
 
+    /**
+     * 判断前台传递过来的值是否匹配当前的枚举值
+     *
+     * @param value 值
+     * @return true:匹配 false:不匹配
+     */
     boolean match(String value);
 }
