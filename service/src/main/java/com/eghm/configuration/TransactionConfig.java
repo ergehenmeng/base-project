03ffe663296659ad99e3ff88f19b180c4fc60ac6
wangyanbing <664956140@qq.com>
2025-01-02 14:33:48 +0800

    优化代码

--- service/src/main/java/com/eghm/configuration/TransactionConfig.java
+++ service/src/main/java/com/eghm/configuration/TransactionConfig.java
@@ -3,6 +3,7 @@ package com.eghm.configuration;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -25,7 +26,7 @@ public class TransactionConfig {
     @Bean
     @Primary
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public TransactionInterceptor txAdvice(@Lazy PlatformTransactionManager transactionManager, TransactionAttributeSource transactionAttributeSource) {
+    public TransactionInterceptor txAdvice(@Lazy PlatformTransactionManager transactionManager, ObjectProvider<TransactionAttributeSource> provider) {
         DefaultTransactionAttribute required = new DefaultTransactionAttribute();
         required.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
         DefaultTransactionAttribute readOnly = new DefaultTransactionAttribute();
@@ -48,7 +49,7 @@ public class TransactionConfig {
         TransactionInterceptor interceptor = new TransactionInterceptor();
         interceptor.setTransactionManager(transactionManager);
         // 设置优先级, 同时支持注解事务和声明式事务
-        interceptor.setTransactionAttributeSources(transactionAttributeSource, attributeSource);
+        interceptor.setTransactionAttributeSources(provider.getIfAvailable(), attributeSource);
         return interceptor;
     }
 
