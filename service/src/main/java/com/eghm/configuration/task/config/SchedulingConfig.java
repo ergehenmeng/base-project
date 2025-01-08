06e23d974441a8c9a1c13c9534c4771f5e2e21f1
wangyanbing <664956140@qq.com>
2025-01-08 13:18:56 +0800

    定时任务优化

--- service/src/main/java/com/eghm/configuration/task/config/SchedulingConfig.java
+++ service/src/main/java/com/eghm/configuration/task/config/SchedulingConfig.java
@@ -3,11 +3,9 @@ package com.eghm.configuration.task.config;
 import com.eghm.annotation.EnableTask;
 import com.eghm.mapper.SysTaskMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.task.ThreadPoolTaskSchedulerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * 开启定时任务{@link EnableTask}后会自动激活该类
@@ -15,7 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
  * @since 2019/9/6 14:49
  */
 @EnableScheduling
-public class TaskConfig implements ThreadPoolTaskSchedulerCustomizer {
+public class TaskConfig {
 
     /**
      * 自定义定时任务bean
@@ -25,8 +23,4 @@ public class TaskConfig implements ThreadPoolTaskSchedulerCustomizer {
         return new SysTaskRegistrar(sysTaskMapper, taskScheduler);
     }
 
-    @Override
-    public void customize(ThreadPoolTaskScheduler taskScheduler) {
-        taskScheduler.setRemoveOnCancelPolicy(true);
-    }
 }
