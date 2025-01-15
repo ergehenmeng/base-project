a0de7e59c2b36398a01f5a4fccbece724289c968
wangyanbing <664956140@qq.com>
2025-01-15 17:32:55 +0800

    删除多余代码

--- service/src/main/java/com/eghm/dto/ext/DateComparator.java
+++ service/src/main/java/com/eghm/dto/ext/DateComparator.java
@@ -16,7 +16,6 @@ public abstract class DateComparator implements Serializable {
     private final transient LocalDateCompare dateCompare = new LocalDateCompare();
 
     @DateCompare
-    @Schema(hidden = true)
     public LocalDateCompare getDateCompare() {
         dateCompare.setStartDate(this.getStartDate());
         dateCompare.setEndDate(this.getEndDate());
