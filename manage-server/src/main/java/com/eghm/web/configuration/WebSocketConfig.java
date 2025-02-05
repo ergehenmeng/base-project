e61728c45e1fc9151289b5705547e78122ad72ae
wangyanbing <664956140@qq.com>
2025-02-05 15:35:21 +0800

    脚本更新

--- manage-server/src/main/java/com/eghm/web/configuration/WebSocketConfig.java
+++ manage-server/src/main/java/com/eghm/web/configuration/WebSocketConfig.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -26,6 +27,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final UserTokenService userTokenService;
 
+    private final ThreadPoolTaskScheduler taskScheduler;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 配置websocket连接地址后缀
@@ -34,7 +37,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("");
+        registry.enableSimpleBroker("").setTaskScheduler(taskScheduler).setHeartbeatValue(new long[]{10000, 20000});
         registry.setApplicationDestinationPrefixes(WEBSOCKET_PREFIX);
     }
 
