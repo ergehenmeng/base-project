99c90138fc2eb52d54b4853f76a516b3533f9bba
wangyanbing <664956140@qq.com>
2025-02-05 09:20:18 +0800

    优化代码

--- webapp-server/src/main/java/com/eghm/web/configuration/filter/ByteHttpRequestFilter.java
+++ webapp-server/src/main/java/com/eghm/web/configuration/filter/ByteHttpRequestFilter.java
@@ -1,17 +1,25 @@
 package com.eghm.web.configuration.filter;
 
 
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.http.Header;
 import com.eghm.configuration.AbstractIgnoreFilter;
+import com.eghm.constants.CommonConstant;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequestWrapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 请求参数重复使用
@@ -29,4 +37,62 @@ public class ByteHttpRequestFilter extends AbstractIgnoreFilter {
         filterChain.doFilter(wrapper, response);
     }
 
+    @Slf4j
+    public static class ByteHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+        private byte[] body;
+
+        ByteHttpServletRequestWrapper(HttpServletRequest request) {
+            super(request);
+        }
+
+        @Override
+        public BufferedReader getReader() throws IOException {
+            return new BufferedReader(new InputStreamReader(getInputStream(), CommonConstant.CHARSET));
+        }
+
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            if (super.getHeader(Header.CONTENT_TYPE.getValue()) == null || super.getHeader(Header.CONTENT_TYPE.getValue()).startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)) {
+                return super.getInputStream();
+            } else {
+                if (this.body == null) {
+                    this.body = this.filterBody(IoUtil.read(super.getInputStream(), StandardCharsets.UTF_8)).getBytes();
+                }
+                final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.body);
+                return new ServletInputStream() {
+
+                    @Override
+                    public boolean isFinished() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isReady() {
+                        return true;
+                    }
+
+                    @Override
+                    public void setReadListener(ReadListener listener) {
+                        log.warn("接受到请求啦, ReadListener不为空");
+                    }
+
+                    @Override
+                    public int read() {
+                        return byteArrayInputStream.read();
+                    }
+                };
+            }
+        }
+
+        /**
+         * 过滤内容
+         *
+         * @param content content
+         * @return 默认不处理
+         */
+        protected @NonNull String filterBody(String content) {
+            return content;
+        }
+    }
 }
