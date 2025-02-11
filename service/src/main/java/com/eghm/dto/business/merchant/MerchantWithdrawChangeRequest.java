08c736483855b8820f9eb1fc69320b2cf23ce96b
wangyanbing <664956140@qq.com>
2025-02-11 16:32:01 +0800

    优化代码

--- service/src/main/java/com/eghm/dto/business/merchant/MerchantWithdrawChangeRequest.java
+++ service/src/main/java/com/eghm/dto/business/merchant/MerchantWithdrawChangeRequest.java
@@ -1,12 +1,8 @@
 package com.eghm.dto.business.merchant;
 
 import com.eghm.enums.WithdrawWay;
-import com.eghm.validation.annotation.OptionInt;
-import com.eghm.validation.annotation.RangeInt;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
