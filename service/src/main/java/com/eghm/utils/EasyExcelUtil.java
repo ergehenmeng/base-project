c07f09da308e6f22cb2597f552fb20c4d3d26a4a
wangyanbing <664956140@qq.com>
2025-03-12 15:25:03 +0800

    删除多余代码

--- service/src/main/java/com/eghm/utils/EasyExcelUtil.java
+++ service/src/main/java/com/eghm/utils/EasyExcelUtil.java
@@ -1,8 +1,7 @@
 package com.eghm.utils;
 
-import cn.hutool.core.util.URLUtil;
+import cn.hutool.core.net.URLEncodeUtil;
 import cn.hutool.http.Header;
-import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
@@ -73,9 +72,9 @@ public class EasyExcelUtil {
             fileName = fileName + ExcelTypeEnum.XLSX.getValue();
         }
         try {
-            response.setHeader(Header.CONTENT_DISPOSITION.getValue(), "attachment;filename=" + URLUtil.encode(fileName, StandardCharsets.UTF_8));
+            response.setHeader(Header.CONTENT_DISPOSITION.getValue(), "attachment;filename=" + URLEncodeUtil.encode(fileName, StandardCharsets.UTF_8));
             response.setContentType(XLSX_CONTENT_TYPE);
-            EasyExcel.write(response.getOutputStream(), cls).sheet(sheetName).registerWriteHandler(FREEZE_ROW_HANDLER).doWrite(rowValues);
+            EasyExcelFactory.write(response.getOutputStream(), cls).sheet(sheetName).registerWriteHandler(FREEZE_ROW_HANDLER).doWrite(rowValues);
         } catch (Exception e) {
             log.error("导出Excel异常 [{}] [{}]", fileName, cls, e);
         }
