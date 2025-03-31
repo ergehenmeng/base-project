d25a6cd9b4babf3807f0d22bfad84e4e02162d35
wangyanbing <664956140@qq.com>
2025-03-31 14:49:11 +0800

    注释调整

--- service/src/main/java/com/eghm/dto/business/order/ticket/TicketOrderQueryRequest.java
+++ service/src/main/java/com/eghm/dto/business/order/ticket/TicketOrderQueryRequest.java
@@ -22,7 +22,7 @@ import java.time.LocalDate;
 @EqualsAndHashCode(callSuper = true)
 public class TicketOrderQueryRequest extends AbstractDatePagingComparator {
 
-    @Schema(description = "订单状态 0:待支付 1:支付中 2:待使用 3:待自提 4:待发货 5:部分发货 6:待收货 7:退款中 8:订单完成 9:已关闭 10:支付异常 11:退款异常")
+    @Schema(description = "订单状态 0:待支付 1:支付中 2:待使用 3:待自提 4:待发货 5:待收货 6:退款中 7:订单完成 8:已关闭 9:支付异常 10:退款异常")
     private OrderState state;
 
     @Schema(description = "开始日期")
