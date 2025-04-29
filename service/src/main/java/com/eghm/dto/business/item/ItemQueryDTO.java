fba1e0fcd16ba63230e090f9713072eb84fee7ac
wangyanbing <664956140@qq.com>
2025-04-29 11:42:33 +0800

    优化代码

--- service/src/main/java/com/eghm/dto/business/item/ItemQueryDTO.java
+++ service/src/main/java/com/eghm/dto/business/item/ItemQueryDTO.java
@@ -14,6 +14,9 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class ItemQueryDTO extends PagingQuery {
 
+    @ApiModelProperty("店铺id")
+    private Long storeId;
+
     @ApiModelProperty(value = "交付方式 1:快递包邮 2:自提")
     private DeliveryType deliveryType;
 
