712b4eb6ca8a056dc7e056edf2bdf4229d071524
wangyanbing <664956140@qq.com>
2025-05-22 17:16:35 +0800

    自提优化

--- service/src/main/java/com/eghm/vo/business/merchant/address/MerchantAddressResponse.java
+++ service/src/main/java/com/eghm/vo/business/merchant/address/MerchantAddressResponse.java
@@ -1,6 +1,7 @@
 package com.eghm.vo.business.merchant.address;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
  */
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MerchantAddressResponse {
 
     @ApiModelProperty(value = "id主键")
