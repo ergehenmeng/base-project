0fa0e5785d751d5179d848906ebc638218815a9d
wangyanbing <664956140@qq.com>
2025-05-26 11:51:45 +0800

    tips

--- manage-server/src/main/java/com/eghm/web/listener/ManageListenerHandler.java
+++ manage-server/src/main/java/com/eghm/web/listener/ManageListenerHandler.java
@@ -4,7 +4,6 @@ import com.eghm.common.AlarmService;
 import com.eghm.common.JsonService;
 import com.eghm.constants.QueueConstant;
 import com.eghm.dto.ext.ItemOrderPayNotify;
-import com.eghm.dto.ext.OrderPayNotify;
 import com.eghm.dto.ext.RefundAudit;
 import com.eghm.dto.ext.SocketMsg;
 import com.eghm.enums.ProductType;
@@ -13,7 +12,6 @@ import com.eghm.model.ManageLog;
 import com.eghm.mq.listener.AbstractListenerHandler;
 import com.eghm.service.business.MemberCouponService;
 import com.eghm.service.sys.ManageLogService;
-import com.google.common.collect.Lists;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -70,7 +68,7 @@ public class ManageListenerHandler extends AbstractListenerHandler {
         log.info("订单支付成功，发送发货通知：[{}]", notify);
         processMessageAck(notify, message, channel, msg -> {
             if (msg.getProductType() == ProductType.ITEM) {
-                simpMessagingTemplate.convertAndSend(WEBSOCKET_PREFIX + "/order/broadcast/" + notify.getMerchantId(), SocketMsg.delivery(Lists.newArrayList(msg.getOrderNo())));
+                simpMessagingTemplate.convertAndSend(WEBSOCKET_PREFIX + "/order/broadcast/" + notify.getMerchantId(), SocketMsg.delivery(msg.getOrderNo()));
             }
         });
     }
@@ -82,7 +80,7 @@ public class ManageListenerHandler extends AbstractListenerHandler {
     public void refundNotice(RefundAudit notify, Message message, Channel channel) throws IOException {
         processMessageAck(notify, message, channel, msg -> {
             if (msg.getOrderNo().startsWith(ProductType.ITEM.getPrefix())) {
-                simpMessagingTemplate.convertAndSend(WEBSOCKET_PREFIX + "/order/broadcast/" + notify.getMerchantId(), SocketMsg.refund(Lists.newArrayList(msg.getOrderNo())));
+                simpMessagingTemplate.convertAndSend(WEBSOCKET_PREFIX + "/order/broadcast/" + notify.getMerchantId(), SocketMsg.refund(msg.getOrderNo()));
             }
         });
     }
