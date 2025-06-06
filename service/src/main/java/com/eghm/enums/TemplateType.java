b8442daa50682d7dd624895ea3305f53a44c818e
wangyanbing <664956140@qq.com>
2025-06-06 11:44:16 +0800

    fix: 短信模板更新

--- service/src/main/java/com/eghm/enums/TemplateType.java
+++ service/src/main/java/com/eghm/enums/TemplateType.java
@@ -24,39 +24,39 @@ public enum TemplateType {
     DEFAULT("default", "自定义短信", "", ""),
 
     /**
-     * 登陆发送短信
+     * 登录(C端)
      */
-    MEMBER_LOGIN("member_login", "登陆发送短信", "", ""),
+    MEMBER_LOGIN("member_login", "登录(C端)", "", "您正在登陆EGHM平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 登陆发送短信
+     * 登录(后台)
      */
-    USER_LOGIN("user_login", "登陆发送短信", "", ""),
+    USER_LOGIN("user_login", "登录(后台)", "", "您正在登陆EGHM管理平台，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 忘记登陆密码
+     * 忘记密码(C端)
      */
-    FORGET("forget", "忘记密码发送短信", "", ""),
+    FORGET("forget", "忘记密码(C端)", "", "您正在修改EGHM平台登录密码，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 注册发送短信
+     * 注册(C端)
      */
-    REGISTER("register", "注册发送短信", "", ""),
+    REGISTER("register", "注册(C端)", "", "您正在注册EGHM平台会员，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
      * 商户解绑微信号短信
      */
-    MERCHANT_UNBIND("merchant_unbind", "商户解绑微信号短信", "", ""),
+    MERCHANT_UNBIND("merchant_unbind", "商户解绑微信号短信", "", "您正在解绑商户微信号，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 更换邮箱发送短信验证码
+     * 更换邮箱(C端)
      */
-    CHANGE_EMAIL("change_email", "更换邮箱发送短信验证码", "", ""),
+    CHANGE_EMAIL("change_email", "更换邮箱(C端)", "", "您正在更换账号邮箱，短信验证码：${param0}。如非本人操作，请忽略此短信"),
 
     /**
-     * 确认无房,发送通知短信
+     * 确认无房
      */
-    CONFIRM_NO_ROOM("confirm_no_room", "确认无房,发送通知短信", "", ""),
+    CONFIRM_NO_ROOM("confirm_no_room", "确认无房", "", "您预订${param0}的房型，经确认该房型已售罄，已做退单处理。订单号：￥{param1}"),
 
     ;
     @EnumValue
