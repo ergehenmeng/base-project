e1aab8fcdd4df542060e1bc183fdef674313cb3d
wangyanbing <664956140@qq.com>
2025-06-13 10:09:45 +0800

    脚本调整

--- manage-server/src/main/java/com/eghm/web/controller/business/StatisticsController.java
+++ manage-server/src/main/java/com/eghm/web/controller/business/StatisticsController.java
@@ -1,6 +1,5 @@
 package com.eghm.web.controller.business;
 
-import com.eghm.annotation.SkipPerm;
 import com.eghm.configuration.security.SecurityHolder;
 import com.eghm.dto.ext.RespBody;
 import com.eghm.dto.statistics.CollectRequest;
@@ -63,7 +62,6 @@ public class StatisticsController {
 
     @GetMapping("/orderDispose")
     @Operation(summary = "订单管理")
-    @SkipPerm
     public RespBody<BusinessDetailVO> orderDispose() {
         BusinessDetailVO statistics = orderService.businessStatistics(SecurityHolder.getMerchantId());
         return RespBody.success(statistics);
@@ -71,7 +69,6 @@ public class StatisticsController {
 
     @GetMapping("/order")
     @Operation(summary = "经营数据")
-    @SkipPerm
     public RespBody<OrderCardVO> order(@ParameterObject DateRequest request) {
         request.setMerchantId(SecurityHolder.getMerchantId());
         OrderCardVO statistics = orderService.orderStatistics(request);
@@ -80,7 +77,6 @@ public class StatisticsController {
 
     @GetMapping("/dayOrder")
     @Operation(summary = "订单统计")
-    @SkipPerm
     public RespBody<List<OrderStatisticsVO>> dayOrder(@ParameterObject DateRequest request) {
         this.setNull(request);
         request.setMerchantId(SecurityHolder.getMerchantId());
@@ -111,7 +107,6 @@ public class StatisticsController {
 
     @GetMapping("/cart")
     @Operation(summary = "加购统计")
-    @SkipPerm
     public RespBody<List<CartStatisticsVO>> cart(@ParameterObject DateRequest request) {
         this.setNull(request);
         request.setMerchantId(SecurityHolder.getMerchantId());
