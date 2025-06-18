7b8ecca7b47f91a092a8f879629797fe519c6466
wangyanbing <664956140@qq.com>
2025-06-18 17:51:46 +0800

    优化代码

--- service/src/main/java/com/eghm/common/impl/SysConfigApi.java
+++ service/src/main/java/com/eghm/common/impl/SysConfigApi.java
@@ -7,7 +7,6 @@ import com.eghm.exception.ParameterException;
 import com.eghm.service.sys.impl.SysConfigServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
