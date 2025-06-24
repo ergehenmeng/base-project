60ace65e4c5492b8c4c6341991260e84054aa157
wangyanbing <664956140@qq.com>
2025-06-24 17:26:22 +0800

    抽奖优化

--- service/src/main/java/com/eghm/vo/business/lottery/LotteryPrizeVO.java
+++ service/src/main/java/com/eghm/vo/business/lottery/LotteryPrizeVO.java
@@ -16,7 +16,7 @@ public class LotteryPrizeVO {
     @ApiModelProperty(value = "奖品名称")
     private String prizeName;
 
-    @ApiModelProperty(value = "中奖商品类型 0:谢谢参与 1:优惠券 2:积分")
+    @ApiModelProperty(value = "中奖商品类型 0:谢谢参与 1:优惠券 2:积分 3:商品")
     private PrizeType prizeType;
 
     @ApiModelProperty(value = "奖品图片")
