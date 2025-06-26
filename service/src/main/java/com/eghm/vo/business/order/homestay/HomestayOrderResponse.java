868898d11f013703547820358ac852f134992de5
wangyanbing <664956140@qq.com>
2025-06-26 15:35:42 +0800

    民宿导出优化

--- service/src/main/java/com/eghm/vo/business/order/homestay/HomestayOrderResponse.java
+++ service/src/main/java/com/eghm/vo/business/order/homestay/HomestayOrderResponse.java
@@ -7,6 +7,7 @@ import com.eghm.convertor.SplitterArraySerializer;
 import com.eghm.convertor.excel.CentToYuanConverter;
 import com.eghm.convertor.excel.EnumExcelConverter;
 import com.eghm.dto.ext.ExcelStyle;
+import com.eghm.enums.ConfirmState;
 import com.eghm.enums.OrderState;
 import com.eghm.enums.PayType;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -30,7 +31,7 @@ public class HomestayOrderResponse extends ExcelStyle {
     private String coverUrl;
 
     @ApiModelProperty("订单编号")
-        @ExcelProperty(value = "订单编号", index = 0)
+    @ExcelProperty(value = "订单编号", index = 0)
     private String orderNo;
 
     @ApiModelProperty("房型名称")
@@ -45,42 +46,46 @@ public class HomestayOrderResponse extends ExcelStyle {
     @ExcelProperty(value = "订单状态", index = 3, converter = EnumExcelConverter.class)
     private OrderState state;
 
+    @ApiModelProperty(value = "订单状态 0:待确认 1:确认有房 2:确认无房 3:自动确认有房")
+    @ExcelProperty(value = "订单状态", index = 4, converter = EnumExcelConverter.class)
+    private ConfirmState confirmState;
+
     @ApiModelProperty("购买数量")
-    @ExcelProperty(value = "购买数量", index = 4)
+    @ExcelProperty(value = "购买数量", index = 5)
     private Integer num;
 
     @ApiModelProperty("联系人姓名")
-    @ExcelProperty(value = "联系人姓名", index = 5)
+    @ExcelProperty(value = "联系人姓名", index = 6)
     private String nickName;
 
     @ApiModelProperty("联系人手机号")
-    @ExcelProperty(value = "联系人手机号", index = 6)
+    @ExcelProperty(value = "联系人手机号", index = 7)
     private String mobile;
 
     @ApiModelProperty("付款金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "付款金额", index = 7, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "付款金额", index = 8, converter = CentToYuanConverter.class)
     private Integer payAmount;
 
     @ApiModelProperty("总优惠金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "总优惠金额", index = 8, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "总优惠金额", index = 9, converter = CentToYuanConverter.class)
     private Integer discountAmount;
 
     @ApiModelProperty("下单时间")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "下单时间", index = 9)
+    @ExcelProperty(value = "下单时间", index = 10)
     private LocalDateTime createTime;
 
     @ApiModelProperty("支付时间")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "支付时间", index = 10)
+    @ExcelProperty(value = "支付时间", index = 11)
     private LocalDateTime payTime;
 
     @ApiModelProperty("支付方式(支付成功才会有支付方式)")
-    @ExcelProperty(value = "支付方式", index = 11, converter = EnumExcelConverter.class)
+    @ExcelProperty(value = "支付方式", index = 12, converter = EnumExcelConverter.class)
     private PayType payType;
 
 }
