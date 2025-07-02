166a949f72a18f2128eb03e288d6fdace969c8a6
wangyanbing <664956140@qq.com>
2025-07-02 17:42:13 +0800

    优化代码

--- manage-server/src/main/java/com/eghm/web/controller/business/ScoreAccountController.java
+++ manage-server/src/main/java/com/eghm/web/controller/business/ScoreAccountController.java
@@ -25,6 +25,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +34,7 @@ import org.springframework.web.bind.annotation.*;
  * @author 二哥很猛
  * @since 2024/2/16
  */
-
+@Slf4j
 @RestController
 @Tag(name="商户积分")
 @AllArgsConstructor
@@ -113,6 +114,7 @@ public class ScoreAccountController {
             String generate = QrCodeUtil.generateAsBase64(prepayVO.getQrCodeUrl(), config, ImgUtil.IMAGE_TYPE_JPG);
             return RespBody.success(generate);
         } catch (Exception e) {
+            log.error("扫码充值创建订单异常", e);
             return RespBody.error(ErrorCode.RECHARGE_CREATE_ERROR);
         }
     }
