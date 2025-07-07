605cdb809223e248c3d6b7c6d20ec4e1b0c8bbd6
wangyanbing <664956140@qq.com>
2025-07-07 16:27:37 +0800

    优化代码

--- service/src/main/java/com/eghm/vo/business/order/homestay/HomestayOrderResponse.java
+++ service/src/main/java/com/eghm/vo/business/order/homestay/HomestayOrderResponse.java
@@ -10,6 +10,7 @@ import com.eghm.dto.ext.ExcelStyle;
 import com.eghm.enums.ConfirmState;
 import com.eghm.enums.OrderState;
 import com.eghm.enums.PayType;
+import com.eghm.enums.RefundState;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -46,46 +47,50 @@ public class HomestayOrderResponse extends ExcelStyle {
     @ExcelProperty(value = "订单状态", index = 3, converter = EnumExcelConverter.class)
     private OrderState state;
 
+    @Schema(description = "退款状态 1:退款申请中 2:退款中 3:退款拒绝 4:退款成功 5:退款失败(该状态和退款中在C端用户看来都是退款中) 6:线下退款(该状态与退款成功在C端用户看来是一样的)")
+    @ExcelProperty(value = "退款状态", index = 4, converter = EnumExcelConverter.class)
+    private RefundState refundState;
+
     @Schema(description = "确认状态 0:待确认 1:确认有房 2:确认无房 3:自动确认有房")
-    @ExcelProperty(value = "确认状态", index = 4, converter = EnumExcelConverter.class)
+    @ExcelProperty(value = "确认状态", index = 5, converter = EnumExcelConverter.class)
     private ConfirmState confirmState;
 
     @Schema(description = "购买数量")
-    @ExcelProperty(value = "购买数量", index = 5)
+    @ExcelProperty(value = "购买数量", index = 6)
     private Integer num;
 
     @Schema(description = "联系人姓名")
-    @ExcelProperty(value = "联系人姓名", index = 6)
+    @ExcelProperty(value = "联系人姓名", index = 7)
     private String nickName;
 
     @Schema(description = "联系人手机号")
-    @ExcelProperty(value = "联系人手机号", index = 7)
+    @ExcelProperty(value = "联系人手机号", index = 8)
     private String mobile;
 
     @Schema(description = "付款金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "付款金额", index = 8, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "付款金额", index = 9, converter = CentToYuanConverter.class)
     private Integer payAmount;
 
     @Schema(description = "总优惠金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "总优惠金额", index = 9, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "总优惠金额", index = 10, converter = CentToYuanConverter.class)
     private Integer discountAmount;
 
     @Schema(description = "下单时间")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "下单时间", index = 10)
+    @ExcelProperty(value = "下单时间", index = 11)
     private LocalDateTime createTime;
 
     @Schema(description = "支付时间")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "支付时间", index = 11)
+    @ExcelProperty(value = "支付时间", index = 12)
     private LocalDateTime payTime;
 
     @Schema(description = "支付方式(支付成功才会有支付方式)")
-    @ExcelProperty(value = "支付方式", index = 12, converter = EnumExcelConverter.class)
+    @ExcelProperty(value = "支付方式", index = 13, converter = EnumExcelConverter.class)
     private PayType payType;
 
 }
--- service/src/main/java/com/eghm/vo/business/order/item/ItemOrderResponse.java
+++ service/src/main/java/com/eghm/vo/business/order/item/ItemOrderResponse.java
@@ -9,6 +9,7 @@ import com.eghm.convertor.excel.EnumExcelConverter;
 import com.eghm.dto.ext.ExcelStyle;
 import com.eghm.enums.OrderState;
 import com.eghm.enums.PayType;
+import com.eghm.enums.RefundState;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -47,45 +48,49 @@ public class ItemOrderResponse extends ExcelStyle {
     @ExcelProperty(value = "订单状态", index = 3, converter = EnumExcelConverter.class)
     private OrderState state;
 
+    @Schema(description = "退款状态 1:退款申请中 2:退款中 3:退款拒绝 4:退款成功 5:退款失败(该状态和退款中在C端用户看来都是退款中) 6:线下退款(该状态与退款成功在C端用户看来是一样的)")
+    @ExcelProperty(value = "退款状态", index = 4, converter = EnumExcelConverter.class)
+    private RefundState refundState;
+
     @Schema(description = "购买数量")
-    @ExcelProperty(value = "购买数量", index = 4)
+    @ExcelProperty(value = "购买数量", index = 5)
     private Integer num;
 
     @Schema(description = "收货人姓名")
-    @ExcelProperty(value = "收货人姓名", index = 5)
+    @ExcelProperty(value = "收货人姓名", index = 6)
     private String nickName;
 
     @Schema(description = "收货人电话")
-    @ExcelProperty(value = "收货人电话", index = 6)
+    @ExcelProperty(value = "收货人电话", index = 7)
     private String mobile;
 
     @Schema(description = "优惠金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "优惠金额", index = 7, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "优惠金额", index = 8, converter = CentToYuanConverter.class)
     private Integer discountAmount;
 
     @Schema(description = "付款金额")
     @JsonSerialize(using = CentToYuanSerializer.class)
-    @ExcelProperty(value = "付款金额", index = 8, converter = CentToYuanConverter.class)
+    @ExcelProperty(value = "付款金额", index = 9, converter = CentToYuanConverter.class)
     private Integer payAmount;
 
     @Schema(description = "下单时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "下单时间", index = 9)
+    @ExcelProperty(value = "下单时间", index = 10)
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
     @Schema(description = "支付时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
-    @ExcelProperty(value = "支付时间", index = 10)
+    @ExcelProperty(value = "支付时间", index = 11)
     private LocalDateTime payTime;
 
     @Schema(description = "支付方式")
-    @ExcelProperty(value = "支付方式", index = 11, converter = EnumExcelConverter.class)
+    @ExcelProperty(value = "支付方式", index = 12, converter = EnumExcelConverter.class)
     private PayType payType;
 
     @Schema(description = "订单类型")
-    @ExcelProperty(value = "订单类型", index = 12)
+    @ExcelProperty(value = "订单类型", index = 13)
     private String orderType;
 }
