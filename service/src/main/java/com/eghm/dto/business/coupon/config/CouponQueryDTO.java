9d3268dc774623cf6cb308dd27f27219074b491c
wangyanbing <664956140@qq.com>
2025-07-10 14:14:36 +0800

    优惠券搜索

--- service/src/main/java/com/eghm/dto/business/coupon/config/CouponQueryDTO.java
+++ service/src/main/java/com/eghm/dto/business/coupon/config/CouponQueryDTO.java
@@ -1,12 +1,9 @@
 package com.eghm.dto.business.coupon.config;
 
 import com.eghm.dto.ext.PagingQuery;
-import com.eghm.enums.CouponType;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import javax.validation.constraints.NotNull;
 
 /**
  * @author 二哥很猛
@@ -17,6 +14,5 @@ import javax.validation.constraints.NotNull;
 public class CouponQueryDTO extends PagingQuery {
 
     @ApiModelProperty(value = "优惠券类型 1:抵扣券 2:折扣券", required = true)
-    @NotNull(message = "优惠券类型不能为空")
-    private CouponType couponType;
+    private Integer couponType;
 }
