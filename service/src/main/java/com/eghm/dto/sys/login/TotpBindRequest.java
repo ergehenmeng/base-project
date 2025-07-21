18e762bd3d3ab1589d1cf2f3e35f0637dbb307b3
wangyanbing <664956140@qq.com>
2025-07-21 15:55:45 +0800

    tips

--- service/src/main/java/com/eghm/dto/sys/login/TotpBindRequest.java
+++ service/src/main/java/com/eghm/dto/sys/login/TotpBindRequest.java
@@ -23,7 +23,7 @@ public class TotpBindRequest {
     @Length(min = 32, max = 32, message = "秘钥格式错误")
     private String secretKey;
 
-    @ApiModelProperty(value = "校验码", required = true)
-    @RangeInt(min = 100000, max = 999999, message = "校验码不合法")
+    @ApiModelProperty(value = "动态口令", required = true)
+    @RangeInt(min = 100000, max = 999999, message = "动态口令不合法")
     private Integer verifyCode;
 }
