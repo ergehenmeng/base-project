011c773618e91c066d671efc59fea941613d239f
wangyanbing <664956140@qq.com>
2025-08-05 16:33:12 +0800

    签名优化

--- webapp-server/src/main/java/com/eghm/web/configuration/interceptor/AccessSignInterceptor.java
+++ webapp-server/src/main/java/com/eghm/web/configuration/interceptor/AccessSignInterceptor.java
@@ -17,7 +17,7 @@ import com.eghm.enums.SignType;
 import com.eghm.exception.BusinessException;
 import com.eghm.utils.WebUtil;
 import com.eghm.vo.operate.auth.AuthConfigVO;
-import com.eghm.web.annotation.SignCheck;
+import com.eghm.web.annotation.AccessSign;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -37,13 +37,13 @@ import static com.eghm.utils.StringUtil.isBlank;
  */
 @Slf4j
 @AllArgsConstructor
-public class SignCheckInterceptor implements InterceptorAdapter {
+public class AccessSignInterceptor implements InterceptorAdapter {
 
     private final CacheProxyService cacheProxyService;
 
     @Override
     public boolean beforeHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
-        SignCheck check = this.getAnnotation(handler, SignCheck.class);
+        AccessSign check = this.getAnnotation(handler, AccessSign.class);
         if (check == null) {
             return true;
         }
