f00287f875d514c15c97287cdf0106d36e701d98
wyb <dianxiaoer@mandofin.com>
2026-05-20 13:21:06 +0800

    文档更新

--- i18n/src/main/java/com/eghm/i18n/provider/I18nMessageProvider.java
+++ i18n/src/main/java/com/eghm/i18n/provider/I18nMessageProvider.java
@@ -22,7 +22,7 @@ public interface I18nMessageProvider {
     /**
      * 通过字典key对消息进行国际化处理
      *
-     * @param message 消息键
+     * @param message 消息键, 可以为错误码, 也可以为普通消息编码, 也可以为数据字典的值
      * @param locale 语言
      * @param key 字典key
      * @return 消息内容
