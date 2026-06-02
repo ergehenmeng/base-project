b7930edd20411e28b73ddfd763562a1e31636e9c
wyb <dianxiaoer@mandofin.com>
2026-06-02 10:47:53 +0800

    删除广播测试方法

--- manage-server/src/main/java/com/eghm/web/controller/WebSocketController.java
+++ manage-server/src/main/java/com/eghm/web/controller/WebSocketController.java
@@ -1,22 +1,11 @@
 package com.eghm.web.controller;
 
-import com.eghm.annotation.SkipPerm;
-import com.eghm.dto.ext.RespBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-import static com.eghm.constants.CommonConstant.WEBSOCKET_PREFIX;
 
 /**
  * @author 二哥很猛
@@ -28,18 +17,9 @@ import static com.eghm.constants.CommonConstant.WEBSOCKET_PREFIX;
 @Tag(name = "websocket消息订阅")
 public class WebSocketController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
-
     @SubscribeMapping("/subscribe/{objectId}")
     public void subscribe(@DestinationVariable("objectId") Long objectId) {
         log.info("[{}]订阅消息成功", objectId);
     }
 
-    @PostMapping("/broadcast/{objectId}")
-    @SkipPerm
-    @Operation(summary = "广播消息测试")
-    public RespBody<Void> broadcast(@PathVariable Long objectId, @RequestBody Map<String, Object> param) {
-        simpMessagingTemplate.convertAndSend(WEBSOCKET_PREFIX + "/broadcast/" + objectId, param);
-        return RespBody.success();
-    }
 }
