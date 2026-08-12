8b5e4bc64802d9f3e33bf5c8e1855475c76dc269
wyb <dianxiaoer@mandofin.com>
2026-08-12 16:09:38 +0800

    优化代码

--- application/webapp-server/src/main/java/com/eghm/app/webapp/controller/DictController.java
+++ application/webapp-server/src/main/java/com/eghm/app/webapp/controller/DictController.java
@@ -1,14 +1,13 @@
 package com.eghm.app.webapp.controller;
 
 import com.eghm.foundation.core.dto.ext.RespBody;
+import com.eghm.foundation.web.utility.DataUtil;
 import com.eghm.platform.config.entity.SysDictItem;
 import com.eghm.platform.config.service.SysDictService;
-import com.eghm.foundation.web.utility.DataUtil;
 import com.eghm.platform.config.vo.BaseItemVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +33,7 @@ public class DictController {
     @GetMapping("/list")
     @Operation(summary = "列表")
     @Parameter(name = "nid", description = "数据字典编号", required = true)
-    public RespBody<List<BaseItemVO>> list(HttpServletRequest request, @RequestParam("nid") String nid) {
+    public RespBody<List<BaseItemVO>> list(@RequestParam("nid") String nid) {
         List<SysDictItem> byPage = sysDictService.getDictByNid(nid);
         return RespBody.success(DataUtil.copy(byPage, BaseItemVO.class));
     }
