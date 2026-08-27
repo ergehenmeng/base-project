26c914b5fc5205f6dabb1680ad9bf96b6af83050
wyb <664956140@qq.com>
2026-08-27 11:44:02 +0800

    序列化翻译

--- starter/i18n-spring-boot-starter/src/main/java/com/eghm/i18n/serializer/TranslationSerializer.java
+++ starter/i18n-spring-boot-starter/src/main/java/com/eghm/i18n/serializer/TranslationSerializer.java
@@ -1,8 +1,10 @@
 package com.eghm.i18n.serializer;
 
+import cn.hutool.core.util.ReflectUtil;
 import com.eghm.i18n.annotation.Translation;
 import com.eghm.i18n.context.LanguageContextHolder;
 import com.eghm.i18n.provider.I18nMessageProvider;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -12,9 +14,14 @@ import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * 兼容jackson枚举序列化翻译
+ *
  * @author wyb-eghm
  * @since 2026/5/15
  */
@@ -23,6 +30,8 @@ public class TranslationSerializer extends StdSerializer<Object> implements Cont
     private volatile static I18nMessageProvider PROVIDER;
     
     private Translation annotation;
+    
+    private static final Map<Class<?>, Field> JSON_VALUE_MAP = new ConcurrentHashMap<>();
     
     public TranslationSerializer() {
         super(Object.class);
@@ -44,16 +53,32 @@ public class TranslationSerializer extends StdSerializer<Object> implements Cont
         if (value == null) {
             gen.writeNull();
         } else {
+            Object result = value;
+            if (value.getClass().isEnum()) {
+                Field valueField = getJsonValueMethod(value.getClass());
+                result = valueField ==  null ? value : ReflectUtil.getFieldValue(value, valueField);
+            }
             if (PROVIDER == null) {
-                gen.writeObject(value);
+                gen.writeObject(result);
             } else {
                 Locale locale = LanguageContextHolder.getLocale();
-                gen.writeString(PROVIDER.getMessage(value.toString(), locale, annotation.value()));
+                gen.writeString(PROVIDER.getMessage(result.toString(), locale, annotation.value()));
             }
         }
     }
     
     public static void setMessageProvider(I18nMessageProvider messageProvider) {
         PROVIDER = messageProvider;
+    }
+    
+    private static Field getJsonValueMethod(Class<?> clazz) {
+        return JSON_VALUE_MAP.computeIfAbsent(clazz, als -> {
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(JsonValue.class)) {
+                    return field;
+                }
+            }
+            return null;
+        });
     }
 }
